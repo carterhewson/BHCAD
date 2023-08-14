481d8a2d9ae9f93ff38a65a07dd26405ecf05edf
carterhewson <140703293+carterhewson@users.noreply.github.com>
2023-08-14 21:08:15 +1200

    Initial Page setup

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <html>
+    <head>
+        <title>Police CAD - Home</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    
+    <div id="banner">
+        <p>This is the header</p>
+    </div>
     <h1>This webpage is under construction</h1>
+    <a href="#">Click here for the UI Example</a>
 </html>
